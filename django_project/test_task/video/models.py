09cbc2c798405e11370b93099fe6abf107509f6c
Alexey Inkov <alex0982@mail.ru>
2024-03-27 01:33:49 +0300

    add models.py VideoFile

--- django_project/test_task/video/models.py
+++ django_project/test_task/video/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class VideoFile(models.Model):
+    run_string = models.CharField(max_length=255)
+    slug = models.SlugField()
+    file = models.FileField()
+
