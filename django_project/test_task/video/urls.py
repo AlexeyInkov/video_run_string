3d7621b09f8a7a0e22f0a9aa8ee4c5e7a055f9cf
Alexey Inkov <alex0982@mail.ru>
2024-03-27 01:32:53 +0300

    update urls

--- django_project/test_task/video/urls.py
+++ django_project/test_task/video/urls.py
@@ -1,12 +1,19 @@
-
 from django.urls import path
 
-from .views import video_run_string
-
+from .views import (
+    video_run_string,
+    VideoListView,
+    VideoCreateView,
+    VideoDetailView,
+    VideoDeleteView,
+)
 
 app_name = "video"
 
-
 urlpatterns = [
-    path("add/", video_run_string, name="create_video"),
+    path("/", VideoListView.as_view(), name="list_video"),
+    path("add/", video_run_string, name="add_video"),
+    path("create/", VideoCreateView.as_view(), name="create_video"),
+    path("<slug:slug>/", VideoDetailView.as_view(), name="detail_video"),
+    path("<slug:slug>/delete/", VideoDeleteView.as_view(), name="delete_video"),
 ]
