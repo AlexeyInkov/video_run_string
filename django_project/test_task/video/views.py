c710eb0c3efa4783d5a6ef8d135ca22d445637c5
Alexey Inkov <alex0982@mail.ru>
2024-03-27 02:00:39 +0300

    add views

--- django_project/test_task/video/views.py
+++ django_project/test_task/video/views.py
@@ -1,9 +1,12 @@
 import os
 
 from django.conf import settings
-from django.http import HttpRequest, HttpResponse, FileResponse, Http404
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, Http404
+from django.urls import reverse_lazy
 
+from django.views.generic import ListView, CreateView, DetailView, DeleteView
+
+from video.models import VideoFile
 from video.utils import create_video_opencv
 
 
@@ -11,12 +14,30 @@ from video.utils import create_video_opencv
 
 
 def video_run_string(request: HttpRequest) -> HttpResponse:
-    text = request.GET.get('text', '')
+    text = request.GET.get("text", "")
     filename = create_video_opencv(text)
     file_path = os.path.join(settings.MEDIA_ROOT, filename)
     if os.path.exists(file_path):
-        with open(file_path, 'rb') as video:
+        with open(file_path, "rb") as video:
             response = HttpResponse(video.read(), content_type="video")
-            response['Content-Disposition'] = 'attachment; filename="%s"' % filename
+            response["Content-Disposition"] = 'attachment; filename="%s"' % filename
             return response
     raise Http404
+
+
+class VideoListView(ListView):
+    queryset = VideoFile.objects.all().order_by("-create_at")
+
+
+class VideoCreateView(CreateView):
+    model = VideoFile
+    fields = ["run_string"]
+
+
+class VideoDetailView(DetailView):
+    model = VideoFile
+
+
+class VideoDeleteView(DeleteView):
+    model = VideoFile
+    success_url = reverse_lazy("author_list")
