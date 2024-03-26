6f00ad774f81081566cbc7b4887d4a00f6d74c58
Alexey Inkov <alex0982@mail.ru>
2024-03-27 02:21:56 +0300

    add admin

--- django_project/test_task/video/admin.py
+++ django_project/test_task/video/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from video.models import VideoFile
+
+
 # Register your models here.
+@admin.register(VideoFile)
+class VideoFileAdmin(admin.ModelAdmin):
+    list_display = (
+        "slug",
+        "run_string",
+        "file",
+        "created_at",
+    )
