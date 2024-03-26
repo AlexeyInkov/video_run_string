11f782338f88af01f53a1be2f12ca12df6719b10
Alexey Inkov <alex0982@mail.ru>
2024-03-27 02:22:36 +0300

    fix VideoFile.created_at

--- django_project/test_task/video/models.py
+++ django_project/test_task/video/models.py
@@ -5,6 +5,6 @@ class VideoFile(models.Model):
     run_string = models.CharField(max_length=255)
     slug = models.SlugField()
     file = models.FileField()
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     objects = models.Manager()
