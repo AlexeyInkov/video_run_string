97185098b5368c2e658f74f3923cac9eb2a52dc4
Alexey Inkov <alex0982@mail.ru>
2024-03-27 22:30:44 +0300

    fix VideoFile models.py

--- django_project/test_task/video/models.py
+++ django_project/test_task/video/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class VideoFile(models.Model):
-    run_string = models.CharField(max_length=255)
-    slug = models.SlugField()
-    file = models.FileField()
+    run_string = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+    file = models.FileField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     objects = models.Manager()
